8d99b892f248dddd07a74e4bd10166ab1c22a84f
jordanjh1 <transcender2012@hotmail.com>
2022-11-27 02:28:09 -0800

    added code for basic movements and image capture

--- image_cap.py
+++ image_cap.py
@@ -9,6 +9,6 @@ me.streamon()
 
 while True:
     img = me.get_frame_read().frame
-    # img = cv2.resize(img, (360, 240))
+    img = cv2.resize(img, (360, 240))
     cv2.imshow("Image", img)
     cv2.waitKey(1)
